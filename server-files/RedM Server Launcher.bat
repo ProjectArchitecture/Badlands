0158d7a98c84dae252293a1b8ad5ceb9ed23ab82
jH <47692789+Avanae@users.noreply.github.com>
2022-04-01 16:02:52 +0200

    Fix indentation on launcher.

--- server-files/RedM Server Launcher.bat
+++ server-files/RedM Server Launcher.bat
@@ -1,6 +1,11 @@
 @ECHO OFF
-TITLE BadlandsRP Red MServer Launcher
+TITLE BadlandsRP RedM Server Launcher
 color 0c
+
+
+echo Launching RedM with the following parameters
+echo set gamename rdr3
+echo set onesync on
 
 cd /d H:\Servers\RedM\[server]\server-data
 H:\Servers\RedM\[server]\server-files\FXServer.exe +exec server.cfg +set gamename rdr3 +set onesync on
